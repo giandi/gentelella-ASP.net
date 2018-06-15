5e77ad850893b6017b70a95989bdfd7625b73a14
nico Louis <luwidgiani@hotmail.com>
2018-06-15 13:44:12 +0200

    Add project files.

---
+++ Beheer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Beheer.MvcApplication" Language="C#" %>
---
+++ Beheer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Beheer.Startup))]
+namespace Beheer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
