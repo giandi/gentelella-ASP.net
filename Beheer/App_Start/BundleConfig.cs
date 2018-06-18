0ad0107ca8a12996c1fc9179e6dd3aef595bae5d
nico Louis <luwidgiani@hotmail.com>
2018-06-18 14:37:47 +0200

    first commit

--- Beheer/App_Start/BundleConfig.cs
+++ Beheer/App_Start/BundleConfig.cs
@@ -8,23 +8,45 @@ namespace Beheer
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/scripts/global").Include(
+                        "~/vendors/jquery/dist/jquery.min.js",
+                        "~/vendors/bootstrap/dist/js/bootstrap.min.js",
+                        "~/vendors/fastclick/lib/fastclick.js",
+                        "~/vendors/nprogress/nprogress.js",
+                        "~/vendors/Chart.js/dist/Chart.min.js",
+                        "~/vendors/gauge.js/dist/gauge.min.js",
+                        "~/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js",
+                        "~/vendors/iCheck/icheck.min.js",
+                        "~/vendors/skycons/skycons.js",
+                        "~/vendors/Flot/jquery.flot.js",
+                        "~/vendors/Flot/jquery.flot.pie.js",
+                        "~/vendors/Flot/jquery.flot.time.js",
+                        "~/vendors/Flot/jquery.flot.stack.js",
+                        "~/vendors/Flot/jquery.flot.resize.js",
+                        "~/vendors/flot.orderbars/js/jquery.flot.orderBars.js",
+                        "~/vendors/flot-spline/js/jquery.flot.spline.min.js",
+                        "~/vendors/flot.curvedlines/curvedLines.js",
+                        "~/vendors/DateJS/build/date.js",
+                        "~/vendors/jqvmap/dist/jquery.vmap.js",
+                        "~/vendors/jqvmap/dist/maps/jquery.vmap.world.js",
+                        "~/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js",
+                        "~/vendors/moment/min/moment.min.js",
+                        "~/vendors/bootstrap-daterangepicker/daterangepicker.js",
+                        "~/scripts/js/custom.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //css
+            bundles.Add(new StyleBundle("~/styles/global").Include(
+                        "~/vendors/bootstrap/dist/css/bootstrap.min.css",
+                        "~/vendors/font-awesome/css/font-awesome.min.css",
+                        "~/vendors/nprogress/nprogress.css",
+                        "~/vendors/iCheck/skins/flat/green.css",
+                        "~/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css",
+                        "~/vendors/jqvmap/dist/jqvmap.min.css",
+                        "~/vendors/bootstrap-daterangepicker/daterangepicker.css",
+                        "~/css/custom.min.css"));
         }
     }
 }
+
+
+
--- Beheer/Controllers/HomeController.cs
+++ Beheer/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace Beheer.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult index()
         {
             return View();
         }
@@ -26,5 +26,286 @@ namespace Beheer.Controllers
 
             return View();
         }
+
+        public ActionResult calendar()
+        {
+            ViewBag.Message = "Your calendar page.";
+
+            return View();
+        }
+
+        public ActionResult chartjs()
+        {
+            ViewBag.Message = "Your chartjs page.";
+
+            return View();
+        }
+
+        public ActionResult chartjs2()
+        {
+            ViewBag.Message = "Your chartjs2 page.";
+
+            return View();
+        }
+
+        public ActionResult contacts()
+        {
+            ViewBag.Message = "Your contacts page.";
+
+            return View();
+        }
+
+        public ActionResult e_commerce()
+        {
+            ViewBag.Message = "Your e_commerce page.";
+
+            return View();
+        }
+
+        public ActionResult echarts()
+        {
+            ViewBag.Message = "Your echarts page.";
+
+            return View();
+        }
+
+        public ActionResult fixed_footer()
+        {
+            ViewBag.Message = "Your fixed_footer page.";
+
+            return View();
+        }
+
+        public ActionResult fixed_sidebar()
+        {
+            ViewBag.Message = "Your fixed_sidebar page.";
+
+            return View();
+        }
+
+        public ActionResult form()
+        {
+            ViewBag.Message = "Your form page.";
+
+            return View();
+        }
+
+        public ActionResult form_advanced()
+        {
+            ViewBag.Message = "Your form_advanced.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult form_buttons()
+        {
+            ViewBag.Message = "Your form_buttons.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult form_upload()
+        {
+            ViewBag.Message = "Your form_upload.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult form_validation()
+        {
+            ViewBag.Message = "Your form_validation.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult form_wizards()
+        {
+            ViewBag.Message = "Your form_wizards.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult general_elements()
+        {
+            ViewBag.Message = "Your general_elements.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult glyphicons()
+        {
+            ViewBag.Message = "Your glyphicons.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult icons()
+        {
+            ViewBag.Message = "Your icons.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult inbox()
+        {
+            ViewBag.Message = "Your inbox.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult index2()
+        {
+            ViewBag.Message = "Your index2.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult index3()
+        {
+            ViewBag.Message = "Your index3.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult invoice()
+        {
+            ViewBag.Message = "Your invoice.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult level2()
+        {
+            ViewBag.Message = "Your level2.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            ViewBag.Message = "Your login.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult map()
+        {
+            ViewBag.Message = "Your map.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult media_gallery()
+        {
+            ViewBag.Message = "Your media_gallery.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult morisjs()
+        {
+            ViewBag.Message = "Your morisjs.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult other_charts()
+        {
+            ViewBag.Message = "Your other_charts.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult page_403()
+        {
+            ViewBag.Message = "Your page_403.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult page_404()
+        {
+            ViewBag.Message = "Your page_404.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult page_500()
+        {
+            ViewBag.Message = "Your page_500.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult plain_page()
+        {
+            ViewBag.Message = "Your plain_page.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult pricing_tables()
+        {
+            ViewBag.Message = "Your pricing_tables.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult profile()
+        {
+            ViewBag.Message = "Your profile.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult project_detail()
+        {
+            ViewBag.Message = "Your project_detail.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult projects()
+        {
+            ViewBag.Message = "Your projects.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult tables()
+        {
+            ViewBag.Message = "Your tables.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult tables_dynamic()
+        {
+            ViewBag.Message = "Your tables_dynamic.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult typography()
+        {
+            ViewBag.Message = "Your typography.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult widgets()
+        {
+            ViewBag.Message = "Your widgets.cshtml page.";
+
+            return View();
+        }
+
+        public ActionResult xx()
+        {
+            ViewBag.Message = "Your xx page.";
+
+            return View();
+        }
+        
     }
 }
